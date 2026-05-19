4c8e8e7967e0f53ca9930915e9f6fae3e37a0342
vishal mahor <vishalmahor353@gmail.com>
2026-05-20 00:38:14 +0530

    fix shorts + transcript error

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.api.routes import router
 from fastapi.middleware.cors import CORSMiddleware
+import os
 
 
 
@@ -18,3 +19,10 @@ app.include_router(router)
 @app.get("/")
 def root():
     return {"message": "API is working 🚀"}
+
+if __name__ == "__main__":
+    import uvicorn
+
+    port = int(os.environ.get("PORT", 8000))
+
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
