c035380273865387d01f2ebdd6040276d5f29d9e
vishal mahor <vishalmahor353@gmail.com>
2026-05-20 13:50:01 +0530

    fix cookies

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     import uvicorn
 
     port = int(os.environ.get("PORT", 10000))  # 🔥 use 10000 default
-
+    print("🚀 STARTING SERVER ON PORT:", port)
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
